1fcf267ae9243dbc527e96153eb073ff8b6df42f
Michael Bocek <mike.bocek@gmail.com>
2025-02-12 20:10:21 -0800

    Updates to theme and config

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,10 +1,10 @@
-AUTHOR = 'Michael Bocek'
-SITENAME = 'Scientific Marginalia'
-SITEURL = ""
+AUTHOR = "Michael Bocek"
+SITENAME = "Scientific Marginalia"
+SITEURL = "https://mikebocek.github.io/scientific-marginalia"
 
-TIMEZONE = 'America/Los_Angeles'
-
-DEFAULT_LANG = 'en'
+TIMEZONE = "America/Los_Angeles"
+PLUGINS = ["render_math"]
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -13,15 +13,18 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-THEME = 'themes/bootstrap2'
-OUTPUT_PATH = 'output'
-PATH = 'content'
+THEME = "themes/elegant"
+OUTPUT_PATH = "output"
+PATH = "content"
+STATIC_PATHS = ["img", "interactive"]
 
 # Custom Home page
-DIRECT_TEMPLATES = (('index', 'blog', 'tags', 'categories', 'archives'))
-TEMPLATE_PAGES = {'home.html': 'index.html',}
+DIRECT_TEMPLATES = ("index", "tags", "categories", "archives")
+TEMPLATE_PAGES = {
+    "index.html": "index.html",
+}
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
